c73d6b556ab4a783484c942776afd0f5d7748f72
Björn <b.pallutt@gmail.com>
2018-10-15 14:43:47 +0200

    json vorspeise

--- src/javascript/jsonLaden.js
+++ src/javascript/jsonLaden.js
@@ -1,13 +1,10 @@
 console.log("halo");
-
-function loadJSON(file,callback) {
-  console.log("loadJSON gestartet")
+//Björn
+let loadJSON = (file,callback) =>{
   let xobj = new XMLHttpRequest();
   xobj.overrideMimeType('application/json');
   xobj.open('GET', file, true);
-  xobj.onreadystatechange = function () {
-  console.log("xobj.readyState="+xobj.readyState);
-  console.log("xobj.status="+xobj.status);
+  xobj.onreadystatechange = () => {
     if (xobj.readyState == 4 && xobj.status == '200') {
     // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
       callback(xobj.responseText);
@@ -15,8 +12,33 @@ function loadJSON(file,callback) {
   };
   xobj.send(null);
 }
-loadJSON('articels.json', function(text){
-    let data = JSON.parse(text);
-    console.log(data);
-    console.log("hallo")
-});
+
+//Björn
+window.onload = function() {
+
+  loadJSON('articels.json', (text) => {
+      let data = JSON.parse(text);
+      console.log(data);
+
+      //Vorspeisen
+      let vorspeisenElement = document.getElementById("Vorspeisen");
+      console.log(vorspeisenElement);
+      let vorspeisen = data.Vorspeisen;
+      console.log(vorspeisen);
+      vorspeisen.forEach(element => {
+        console.log(element)
+        let divElement = document.createElement("div");
+        vorspeisenElement.appendChild(divElement);
+
+        let imgElement = document.createElement("img");
+        imgElement.src = "img/"+element.image;
+        divElement.appendChild(imgElement);
+
+        let txtNode = document.createTextNode(element.Beschreibung);
+        divElement.appendChild(txtNode);
+
+      });
+
+
+  });
+};
