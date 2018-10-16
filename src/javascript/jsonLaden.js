4b71ecc3b177dc08da1972fb9f65df8710a5f2e4
Björn <b.pallutt@gmail.com>
2018-10-16 11:23:37 +0200

    Einlesung der gesamten JSON Datei und einfügung in die DOM

--- src/javascript/jsonLaden.js
+++ src/javascript/jsonLaden.js
@@ -20,6 +20,45 @@ window.onload = function() {
       let data = JSON.parse(text);
       console.log(data);
 
+      let list = document.getElementById("list");
+      let scrollspy = document.getElementById("main");
+      data.forEach(group => {
+        let divGroup = document.createElement("div");
+
+        let groupRef = document.createElement("a");
+        groupRef.classList.add("list-group-item");
+        groupRef.classList.add("list-group-item-action");
+        groupRef.href= "#"+group.groupname;
+        groupRef.textContent = group.groupname;
+        list.appendChild(groupRef);
+
+        let groupTitle = document.createElement("h4");
+        groupTitle.id = group.groupname;
+        groupTitle.textContent = group.groupname;
+        divGroup.appendChild(groupTitle);
+
+        let groupList = group.list;
+        groupList.forEach(listItem => {
+          let divElement = document.createElement("div");
+
+          let divBeschreibung = document.createElement("div");
+          divBeschreibung.innerHTML =
+            "<b>"+listItem.name+"</b> <br>"+listItem.beschreibung;
+          divElement.appendChild(divBeschreibung);
+
+          let spanPreis = document.createElement("span");
+          spanPreis.textContent= listItem.preis;
+          divElement.appendChild(spanPreis);
+
+          let imgElement = document.createElement("img");
+          imgElement.src = "img/"+listItem.image;
+          divElement.appendChild(imgElement);
+
+          divGroup.appendChild(divElement);
+        })
+        scrollspy.appendChild(divGroup);
+      })
+/**
       //Vorspeisen
       let vorspeisenElement = document.getElementById("Vorspeisen");
       console.log(vorspeisenElement);
@@ -39,6 +78,6 @@ window.onload = function() {
 
       });
 
-
+**/
   });
 };
