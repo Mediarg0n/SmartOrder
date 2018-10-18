058326a222b8cd2e54380d5a1c42a2e025eb560e
Björn <b.pallutt@gmail.com>
2018-10-18 12:34:33 +0200

    autocomplete funktioniert/ verlinkung fehlt noch

--- src/javascript/autocomplete.js
+++ src/javascript/autocomplete.js
@@ -6,28 +6,41 @@ autocomplete = (JSONDATA) =>{
   //  'use strict';
 
     let groups = [/*{name="Vorspeisen", items=[] ,refs=[] }*/];
+    let g;
+    JSONDATA.forEach(group => {
+      let groupname = group.groupname;
+      let item = [];
+      group.list.forEach(listItem => {
+        item.push(listItem.name);
+      });
+      console.log(item);
+      groups.push($.map(item, (g) =>{ return { value: g, data: { category: group.groupname}}}));
+      if(groups.length==1)
+        g = groups[groups.length-1];
+      else
+        g = g.concat(groups[groups.length-1]);
+    });
 
-    console.log(JSONDATA);
 
 /*  let divGroups = document.querySelectorAll("div.divGroup");
-    console.log(divGroups);
+    conscole.log(divGroups);
     for (let i=0;i<divGroups.length;i++){
       console.log(divGroups.item(i).outerHTML);
     }
     console.log(divGroups.item(0).outerHTML);
-*/
+
     var nhlTeams = ['Anaheim Ducks', 'Atlanta Thrashers', 'Boston Bruins', 'Buffalo Sabres', 'Calgary Flames', 'Carolina Hurricanes', 'Chicago Blackhawks', 'Colorado Avalanche', 'Columbus Blue Jackets', 'Dallas Stars', 'Detroit Red Wings', 'Edmonton OIlers', 'Florida Panthers', 'Los Angeles Kings', 'Minnesota Wild', 'Montreal Canadiens', 'Nashville Predators', 'New Jersey Devils', 'New Rork Islanders', 'New York Rangers', 'Ottawa Senators', 'Philadelphia Flyers', 'Phoenix Coyotes', 'Pittsburgh Penguins', 'Saint Louis Blues', 'San Jose Sharks', 'Tampa Bay Lightning', 'Toronto Maple Leafs', 'Vancouver Canucks', 'Washington Capitals'];
     var nbaTeams = ['Atlanta Hawks', 'Boston Celtics', 'Charlotte Bobcats', 'Chicago Bulls', 'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons', 'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'LA Clippers', 'LA Lakers', 'Memphis Grizzlies', 'Miami Heat', 'Milwaukee Bucks', 'Minnesota Timberwolves', 'New Jersey Nets', 'New Orleans Hornets', 'New York Knicks', 'Oklahoma City Thunder', 'Orlando Magic', 'Philadelphia Sixers', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Toronto Raptors', 'Utah Jazz', 'Washington Wizards'];
     var nhl = $.map(nhlTeams, (team) =>{ return { value: team, data: { category: 'NHL', info:"test" }}; });
     var nba = $.map(nbaTeams, (team) =>{ return { value: team, data: { category: 'NBA' } }; });
     var teams = nhl.concat(nba);
-
+*/
     // Initialize autocomplete with local lookup:
     $('#autocomplete').devbridgeAutocomplete({
-        lookup: teams,
+        lookup: g,
         minChars: 1,
         onSelect: (suggestion)=> {
-          console.log(suggestion.data.info)
+          console.log(suggestion.data)
           //  $('#selection').html('You selected: ' + suggestion.value + ', ' + suggestion.data.category);
         },
         showNoSuggestionNotice: true,
--- src/javascript/jsonLaden.js
+++ src/javascript/jsonLaden.js
@@ -24,7 +24,6 @@ window.addEventListener("load", ()=> {
       let JSONDATA = JSON.parse(text);
       console.log(JSONDATA);
       console.log("loadJSON");
->>>>>>> 9ea37f91e193ffc6cc3ceab4f78531ae95223a2d
       //Beispiel für Einfügen
       JSONDATA.splice(1,0,  {
           "groupname" : "Essen",
@@ -67,7 +66,7 @@ window.addEventListener("load", ()=> {
         let groupList = group.list;
         groupList.forEach(listItem => {
           let divElement = document.createElement("div");
-          divElement.id = listItem.name;
+          divElement.id = listItem.name.replace(/ /g, '_');
           divElement.classList.add("Artikel");
 
           let hArtikelName = document.createElement("h5");
