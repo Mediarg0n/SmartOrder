288bf5387651edd6b0cfe28d4dbb57658512ddba
DeurerJ <43781343+DeurerJ@users.noreply.github.com>
2018-11-18 18:56:11 +0100

    Tischauswahl gestrichen
    Funktionalität war nur beschränkt möglich
    
    Co-Authored-By: nickla76 <nickla76@users.noreply.github.com>

--- src/javascript/passwordcheck.js
+++
@@ -1,9 +0,0 @@
-<script>
-function checkPassword(){
-var msg1 = document.querySelector("#username").value;
-var msg2 = document.querySelector("#password").value;
-if(msg1=='Test'&& msg2=='Test')
-window.open('index.html');
-else
-alert("Wrong")}
-</script>
